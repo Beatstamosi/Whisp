6287061da5c4d233c899a5c4470cafbaf3a98bec
Beatstamosi <moritz.lued@gmail.com>
2025-10-23 19:42:46 +0100

    Add: CORS for railway

--- server/src/index.ts
+++ server/src/index.ts
@@ -29,13 +29,24 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(passport.initialize());
 
-// Enable CORS only during development
+// Enable CORS during development
 if (process.env.NODE_ENV === "development") {
   app.use(
     cors({
       origin: "http://localhost:5173",
       credentials: true,
       methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    })
+  );
+}
+
+if (process.env.NODE_ENV === "production") {
+  app.use(
+    cors({
+      origin: ["https://whisp-front-end-production.up.railway.app"],
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: true,
     })
   );
 }
